7ea4b52ae36175772d512f721e01de419093222e
Julia Mindlin <tabu@sci1.jasmin.ac.uk>
2023-04-14 16:28:18 +0100

    merge diagnostic

--- diagnostic_mergtime.sh
+++ diagnostic_mergtime.sh
@@ -5,13 +5,15 @@ path='/gws/nopw/j04/ncas_generic/users/jmindlin/output/EP_fluxes_yearly_20230413
 pathout='/home/users/tabu/eunpa_lim_project/EP_FLUX_DATA'
 #ACCESS-CM2 ACCESS-ESM1-5 AWI-CM-1-1-MR BCC-ESM1 CanESM5-CanOE CanESM5 CanESM5
 
+module load jaspy
+
 diagnostics=(momentum_flux_div momentum_fluxes heat_fluxes heat_flux_div)
 
 for model in AWI-ESM-1-1-LR CESM2-WACCM-FV2 GISS-E2-1-G_r1i1p1f1 IITM-ESM MPI-ESM1-2-HR CanESM5 CNRM-CM6-1 GISS-E2-1-G_r1i1p1f2 IPSL-CM6A-LR MPI-ESM1-2-LR CESM2-FV2 CNRM-CM6-1-HR HadGEM3-GC31-LL KACE-1-0-G MRI-ESM2-0 CESM2-WACCM CNRM-ESM2-1 HadGEM3-GC31-MM MPI-ESM-1-2-HAM NESM3 NorESM2-LM NorESM2-MM SAM0-UNICON TaiESM1 UKESM1-0-LL
 do
 	for diagnostic in ${diagnostics[@]}
 	do	
-		list=(`ls ${path}/${model}/${diagnostic}/*.nc`)
+		list=(`ls ${path}/${model}/${diagnostic}/all_times/*.nc`)
 	        startfile="${list[0]}#"
         	endfile="${list[-1]}#"
         	startname=${startfile##*/}
@@ -27,10 +29,9 @@ do
         	IFS='-' read -r -a endt <<< "$end"
         	enddate=${endt[-1]}
         	base=${startname%_*_*}
-        	mkdir ${path}/${model}/${diagnostic}/all_times
-        	cdo mergetime ${path}/${model}/${diagnostic}/*.nc $path/${model}/${diagnostic}/${base}_$startdate-$enddate"_T42.nc"
-	        mv $path/${model}/${diagnostic}/*19*19* $path/${model}/${diagnostic}/*20*20* ${path}/${model}/${diagnostic}/all_times
-d	
+        	#mkdir ${path}/${model}/${diagnostic}/all_times
+        	cdo mergetime ${path}/${model}/${diagnostic}/all_times/*.nc $path/${model}/${diagnostic}/${base}_$startdate-$enddate"_T42.nc"
+	        #mv $path/${model}/${diagnostic}/*19*19* $path/${model}/${diagnostic}/*20*20* ${path}/${model}/${diagnostic}/all_times
 done; done
 
 
